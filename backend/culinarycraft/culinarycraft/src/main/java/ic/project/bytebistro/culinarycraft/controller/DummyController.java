038cda1e9d52f0a7f6a2dc14594a2556ca7bbc41
cristian <cojocaru.cristian20@gmail.com>
2024-03-10 16:36:39 +0200

    Setup backend config

---
+++ backend/culinarycraft/culinarycraft/src/main/java/ic/project/bytebistro/culinarycraft/controller/DummyController.java
@@ -0,0 +1,16 @@
+package ic.project.bytebistro.culinarycraft.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class DummyController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello world!";
+    }
+
+}
---
+++ backend/culinarycraft/culinarycraft/src/test/java/ic/project/bytebistro/culinarycraft/CulinarycraftApplicationTests.java
@@ -0,0 +1,13 @@
+package ic.project.bytebistro.culinarycraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulinarycraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
