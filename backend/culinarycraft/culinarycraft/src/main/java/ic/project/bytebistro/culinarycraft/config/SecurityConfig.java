390d04efe2ce3887d7f3d008fce2c1f58dd30cbb
cristian <cojocaru.cristian20@gmail.com>
2024-03-12 01:42:56 +0200

    Setup oauth2 security config

---
+++ backend/culinarycraft/culinarycraft/src/main/java/ic/project/bytebistro/culinarycraft/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package ic.project.bytebistro.culinarycraft.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests( auth -> {
+                    auth.requestMatchers("/api/v1/hello").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .oauth2Login(withDefaults())
+                .build();
+    }
+}
