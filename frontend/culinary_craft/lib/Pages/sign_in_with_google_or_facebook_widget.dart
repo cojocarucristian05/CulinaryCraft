ebb5c5586dbf9074a681ac46f89ed349b1ccc3b8
cebucmihai <mihaicebuc36@gmail.com>
2024-03-19 18:29:40 +0200

    Added Sign In page.

--- frontend/culinary_craft/lib/Pages/sign_in_with_google_or_facebook_widget.dart
+++ frontend/culinary_craft/lib/Pages/sign_in_with_google_or_facebook_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -120,7 +121,8 @@ class _SignInWithGoogleOrFacebookWidgetState
                           children: [
                             ElevatedButton(
                               onPressed: () async {
-                                // Handle sign in
+                                HapticFeedback.lightImpact();
+                                Navigator.of(context).pushNamed('/signin');
                               },
                               style: ButtonStyle(
                                 minimumSize: MaterialStateProperty.all(
--- frontend/culinary_craft/lib/main.dart
+++ frontend/culinary_craft/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:culinary_craft_wireframe/Pages/onboarding_slideshow_widget.dart';
 import 'package:flutter/material.dart';
 import 'Pages/get_started_widget.dart';
+import 'Pages/sign_in_widget.dart';
 import 'Pages/sign_in_with_google_or_facebook_widget.dart';
 
 void main() => runApp(MaterialApp(
@@ -9,5 +10,6 @@ void main() => runApp(MaterialApp(
     '/home': (context) => GetStartedWidget(),
     '/onboarding': (context) => OnboardingSlideshowWidget(),
     '/signin_with_google_or_facebook': (context) => SignInWithGoogleOrFacebookWidget(),
+    '/signin': (context) => SignInWidget(),
   },
 ));
